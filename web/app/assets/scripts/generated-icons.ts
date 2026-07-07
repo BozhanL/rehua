cadbffa69efdc7429bc1c93269e0e571a7aef4cd
as178 <shadowangel753@gmail.com>
2026-07-07 14:23:34 +0000

    refactor(web): tidy-ups and linting

---
+++ web/app/assets/scripts/generated-icons.ts
@@ -0,0 +1,90 @@
+// AUTO-GENERATED FILE, DO NOT MODIFY !!!
+import Access from '../icons/access.svg';
+import Alert from '../icons/alert.svg';
+import ButtonAccessArrow from '../icons/button-access-arrow.svg';
+import Calendar from '../icons/calendar.svg';
+import Camera from '../icons/camera.svg';
+import CheckedBox from '../icons/checked-box.svg';
+import CircleArrow from '../icons/circle-arrow.svg';
+import Clipboard from '../icons/clipboard.svg';
+import CrossedEye from '../icons/crossed-eye.svg';
+import Eye from '../icons/eye.svg';
+import FileUpload from '../icons/file-upload.svg';
+import File from '../icons/file.svg';
+import FolderOpen from '../icons/folder-open.svg';
+import FolderPlus from '../icons/folder-plus.svg';
+import Folder from '../icons/folder.svg';
+import HeartPulse from '../icons/heart-pulse.svg';
+import InfoCircle from '../icons/info-circle.svg';
+import InfoSquare from '../icons/info-square.svg';
+import Key from '../icons/key.svg';
+import LockTime from '../icons/lock-time.svg';
+import Lock from '../icons/lock.svg';
+import ManualTick from '../icons/manual-tick.svg';
+import Manual from '../icons/manual.svg';
+import Patient from '../icons/patient.svg';
+import PencilNote from '../icons/pencil-note.svg';
+import PencilSign from '../icons/pencil-sign.svg';
+import Pencil from '../icons/pencil.svg';
+import Piechart from '../icons/piechart.svg';
+import Pin from '../icons/pin.svg';
+import Plus from '../icons/plus.svg';
+import Save from '../icons/save.svg';
+import Search from '../icons/search.svg';
+import SectionPlus from '../icons/section-plus.svg';
+import SimpleArrow from '../icons/simple-arrow.svg';
+import Tick from '../icons/tick.svg';
+import Trash from '../icons/trash.svg';
+import UserFolder from '../icons/user-folder.svg';
+import UserPlus from '../icons/user-plus.svg';
+import UserProfile from '../icons/user-profile.svg';
+import User from '../icons/user.svg';
+import Users from '../icons/users.svg';
+import VersionHistory from '../icons/version-history.svg';
+import X from '../icons/x.svg';
+
+export const icons = {
+  access: Access,
+  alert: Alert,
+  'button-access-arrow': ButtonAccessArrow,
+  calendar: Calendar,
+  camera: Camera,
+  'checked-box': CheckedBox,
+  'circle-arrow': CircleArrow,
+  clipboard: Clipboard,
+  'crossed-eye': CrossedEye,
+  eye: Eye,
+  'file-upload': FileUpload,
+  file: File,
+  'folder-open': FolderOpen,
+  'folder-plus': FolderPlus,
+  folder: Folder,
+  'heart-pulse': HeartPulse,
+  'info-circle': InfoCircle,
+  'info-square': InfoSquare,
+  key: Key,
+  'lock-time': LockTime,
+  lock: Lock,
+  'manual-tick': ManualTick,
+  manual: Manual,
+  patient: Patient,
+  'pencil-note': PencilNote,
+  'pencil-sign': PencilSign,
+  pencil: Pencil,
+  piechart: Piechart,
+  pin: Pin,
+  plus: Plus,
+  save: Save,
+  search: Search,
+  'section-plus': SectionPlus,
+  'simple-arrow': SimpleArrow,
+  tick: Tick,
+  trash: Trash,
+  'user-folder': UserFolder,
+  'user-plus': UserPlus,
+  'user-profile': UserProfile,
+  user: User,
+  users: Users,
+  'version-history': VersionHistory,
+  x: X,
+} as const;
