e26b872f04b63a82aa0ae0847de2e65beeae0599
as178 <shadowangel753@gmail.com>
2026-07-07 08:52:33 +0000

    feat(web): development of reusable Icon component

--- web/app/components/Icon.tsx
+++ web/app/components/Icon.tsx
@@ -1,16 +1,49 @@
-// import { icons } from '../assets/scripts/generated-icons';
-// import type { JSX } from 'react/jsx-runtime';
+import { icons } from '../assets/scripts/generated-icons';
+import type { JSX, SVGProps } from 'react';
 
-// type IconProps = Omit<
-//   React.SVGProps<SVGSVGElement>,
-//   'width' | 'height' | 'ref'
-// > & {
-//   name: keyof typeof icons;
-//   size?: number;
-//   rotation?: number;
-//   flip?: 'horizontal' | 'vertical';
-// };
+// type that is a subset of SVGProps, omitting some props to ensure consistent aspect ratio
+type IconProps = Omit<SVGProps<SVGSVGElement>, 'width' | 'height' | 'ref'> & {
+  // name (mandatory) of the icon to render, must be a key in the icons object
+  // width is optional, fallback to 24px wide
+  // rotation is optional, in degrees, fallback to 0
+  name: keyof typeof icons;
+  width?: number;
+  rotation?: number;
+  flip?: 'horizontal' | 'vertical';
+};
 
-// function Icon(): JSX.Element {
-//   return <TestIcon />;
-// }
+// React component that renders an SVG icon with a consistent aspect ratio
+function Icon({
+  name,
+  width = 24,
+  rotation = 0,
+  flip,
+  style,
+  ...props
+}: IconProps): JSX.Element {
+  const SvgIcon = icons[name];
+
+  const transformations = [
+    rotation ? `rotate(${String(rotation)}deg)` : '',
+    flip === 'horizontal' ? 'scaleX(-1)' : '',
+    flip === 'vertical' ? 'scaleY(-1)' : '',
+  ]
+    .filter(Boolean)
+    .join(' ');
+
+  return (
+    <SvgIcon
+      {...props}
+      width={width}
+      preserveAspectRatio="xMidYMid meet" // ensures the icon scales correctly without distortion
+      style={{
+        ...style,
+        transform: transformations || undefined,
+        transformOrigin: 'center', // rotation and flips occur around the center of the icon
+        transformBox: 'fill-box', // transform origin is relative to SVG's bounding box
+      }}
+    />
+  );
+}
+
+export default Icon;
