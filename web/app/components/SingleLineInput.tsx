a7df289b46f651888fca556adede87fd2de983c6
as178 <shadowangel753@gmail.com>
2026-07-29 15:09:37 +0000

    refactor(web): added comments to explain function and styling

--- web/app/components/SingleLineInput.tsx
+++ web/app/components/SingleLineInput.tsx
@@ -1,5 +1,6 @@
 import type { InputHTMLAttributes, JSX } from 'react';
 
+// React component that renders single line input, with flexible props and pre-set styling
 function SingleLineInput({
   style,
   ...props
@@ -10,12 +11,12 @@ function SingleLineInput({
       className="bg-rehua-white"
       style={{
         width: '100%',
-        fontSize: 24,
-        padding: '0 10px',
+        fontSize: 24, // by default, height is determined based on font size
+        padding: '0 10px', // spacing of the first letter and the front of the input box
         border: '1px solid',
         borderRadius: 6,
         boxShadow: 'inset 0 1px 3px rgb(0 0 0 / 0.3)',
-        outline: 'none',
+        outline: 'none', // no outline should appear upon selection
         ...style,
       }}
     />
