769808e8ff0463a316b9cb83ab931d5da4e26c48
waniven <waniven@icloud.com>
2026-08-18 15:56:36 +0000

    feat(api): changed all methods to use db user instead of temporary hardcoded user
    type issue with the controller as userdocument dose not seem to be acceptable, will need to find
    another workaround to enable the routes to use the user and have the _id value
    
    BREAKING CHANGE: type issue with the controller as userdocument dose not seem to be acceptable, will
    need to find another workaround to enable the routes to use the user and have the _id value

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { AuthService } from './auth.service';
 import { JWT_SECRET, JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
 import { TOTPStrategy } from './totp.strategy';
-import { UsersModule } from '@/users/users.module';
+import { UserModule } from '@/schema/users/user.module';
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
@@ -14,7 +14,7 @@ const JWT_SIGN_OPTIONS: SignOptions = { expiresIn: '1h' };
 @Module({
   controllers: [AuthController],
   imports: [
-    UsersModule,
+    UserModule,
     PassportModule,
     JwtModule.register({
       secret: JWT_SECRET,
