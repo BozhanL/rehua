d33fbd5d7cc8fea5ae6c1040e27f0deae01f4cda
waniven <waniven@icloud.com>
2026-08-20 09:31:36 +0000

    fix(api): fixed the auth route to work with db

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -6,7 +6,6 @@ import { EmergencyContactModule } from './schema/emergency_contacts/emergency_co
 import { ObservationModule } from './schema/observations/observation.module';
 import { PatientModule } from './schema/patients/patient.module';
 import { UserModule } from './schema/users/user.module';
-import { UsersModule } from './users/users.module';
 import { Config, https } from './utils/config';
 import { DynamicModule, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
