cc3b38e111c51953675022f815d1b14ea143f4f8
waniven <waniven@icloud.com>
2026-08-20 09:31:36 +0000

    fix(api): fixed the auth route to work with db

--- api/src/utils/types.ts
+++
@@ -1,9 +0,0 @@
-import type { UserSchema } from '@/users/users.service';
-
-export type ExpressUser = Omit<UserSchema, 'password' | 'totpSecret'>;
-
-declare module 'express' {
-  interface Request {
-    user?: ExpressUser | undefined;
-  }
-}
