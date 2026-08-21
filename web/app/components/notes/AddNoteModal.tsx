6397756fb3ac0781512fcb879c719eb89ef3f387
as178 <shadowangel753@gmail.com>
2026-08-21 00:44:45 +0000

    feat(web): added in pop-ups for no new changes/additions (old TODOs)

--- web/app/components/notes/AddNoteModal.tsx
+++ web/app/components/notes/AddNoteModal.tsx
@@ -4,6 +4,7 @@ import ContentButton from '../common/ContentButton';
 import Icon from '../common/Icon';
 import Modal from '../common/Modal';
 import MultiLineInput from '../common/MultiLineInput';
+import PopUp from '../common/PopUp';
 import { useState, type JSX } from 'react';
 
 // interface for AddNoteModal, defines props for controlling modal visibility, closing modal, and adding a new note
@@ -57,13 +58,16 @@ function AddNoteModal({
 }: Readonly<AddNoteModalProps>): JSX.Element {
   // state to hold text input for new note
   const [text, setText] = useState('');
+  // state to control visibility of pop-up when there are no new notes to save
+  const [showNoChangesPopUp, setShowNoChangesPopUp] = useState(false);
 
   // function to handle adding a new note; trims whitespace and checks for empty input before calling onAdd callback
   function handleAdd(): void {
     const plainText = text.trim();
 
-    // if there are no changes, do not call onAdd (TODO: pop up should be displayed to inform user that note cannot be empty)
+    // if there are no new notes, do not call onAdd & display pop-up to inform user
     if (!plainText) {
+      setShowNoChangesPopUp(true);
       return;
     }
 
@@ -76,45 +80,75 @@ function AddNoteModal({
   }
 
   return (
-    <Modal open={open}>
-      <div className="flex h-full flex-col gap-7 p-8">
-        {/* modal top row content */}
-        <div className="flex items-center gap-6">
-          <button type="button" onClick={onClose} style={{ cursor: 'pointer' }}>
-            <Icon name="circle-arrow" width={65} className="text-rehua-navy" />
-          </button>
-          <Icon name="clipboard" width={45} className="text-rehua-maroon" />
-          <div className="font-bold text-rehua-maroon" style={{ fontSize: 45 }}>
-            Running Notes: Add New Entry
+    <>
+      {/* pop-up for when there are no new notes to save */}
+      <PopUp
+        text1={
+          'No notes have been entered.\nPlease type in notes before submitting the entry.'
+        }
+        text1Style={{ lineHeight: 1.3 }}
+        button1Props={{
+          text1: 'OK',
+          iconProps: { name: 'circle-arrow' },
+          backgroundColor: 'bg-rehua-green',
+          horizontalPadding: 0.3,
+          onClick: () => {
+            setShowNoChangesPopUp(false);
+          },
+        }}
+        modalProps={{ open: showNoChangesPopUp, surfaceProps: { height: 600 } }}
+      />
+      <Modal open={open}>
+        <div className="flex h-full flex-col gap-7 p-8">
+          {/* modal top row content */}
+          <div className="flex items-center gap-6">
+            <button
+              type="button"
+              onClick={onClose}
+              style={{ cursor: 'pointer' }}
+            >
+              <Icon
+                name="circle-arrow"
+                width={65}
+                className="text-rehua-navy"
+              />
+            </button>
+            <Icon name="clipboard" width={45} className="text-rehua-maroon" />
+            <div
+              className="font-bold text-rehua-maroon"
+              style={{ fontSize: 45 }}
+            >
+              Running Notes: Add New Entry
+            </div>
+          </div>
+
+          {/* multi-line input for new note text */}
+          <div className="flex-1">
+            <MultiLineInput
+              value={text}
+              placeholder="Enter new note here . . ."
+              style={{ height: '100%', fontSize: 25 }}
+              // update text state on input change
+              onChange={(e) => {
+                setText(e.target.value);
+              }}
+            />
+          </div>
+
+          {/* add new note button */}
+          <div className="flex justify-end gap-3">
+            <ContentButton
+              text1="Add New Note"
+              height={50}
+              iconProps={{ name: 'plus' }}
+              backgroundColor="bg-rehua-green"
+              verticalPadding={0.2}
+              onClick={handleAdd}
+            />
           </div>
         </div>
-
-        {/* multi-line input for new note text */}
-        <div className="flex-1">
-          <MultiLineInput
-            value={text}
-            placeholder="Enter new note here . . ."
-            style={{ height: '100%', fontSize: 25 }}
-            // update text state on input change
-            onChange={(e) => {
-              setText(e.target.value);
-            }}
-          />
-        </div>
-
-        {/* add new note button */}
-        <div className="flex justify-end gap-3">
-          <ContentButton
-            text1="Add New Note"
-            height={50}
-            iconProps={{ name: 'plus' }}
-            backgroundColor="bg-rehua-green"
-            verticalPadding={0.2}
-            onClick={handleAdd}
-          />
-        </div>
-      </div>
-    </Modal>
+      </Modal>
+    </>
   );
 }
 
