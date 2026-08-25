750206eb992e1755bbf70527cfda6a25e4efda90
as178 <shadowangel753@gmail.com>
2026-08-25 06:07:03 +0000

    feat(web): modified demo/testing content

--- web/app/demos/components/navigation/page.tsx
+++ web/app/demos/components/navigation/page.tsx
@@ -4,5 +4,5 @@ import NavigationBar from '@/app/components/navigation/NavigationBar';
 import type { JSX } from 'react';
 
 export default function NavigationTestPage(): JSX.Element {
-  return <NavigationBar firstName="Angela" lastName="Saric" group="admin" />;
+  return <NavigationBar firstName="Jane" lastName="Smith" group="admin" />;
 }
