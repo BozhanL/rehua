b2233dd27adeca294fd5a44f6e6374db6446356b
Bozhan Liang <lbz@rivensbane.com>
2026-08-27 03:48:32 +0000

    feat(api): remove update-template.dto

--- api/src/templates/dto/update-template.dto.ts
+++
@@ -1,4 +0,0 @@
-import { CreateTemplateDto } from './create-template.dto';
-import { PartialType } from '@nestjs/swagger';
-
-export class UpdateTemplateDto extends PartialType(CreateTemplateDto) {}
