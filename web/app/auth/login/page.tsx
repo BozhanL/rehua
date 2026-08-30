490bc11fac95fd315a9bea5b3aec82c04de9925b
waniven <waniven@icloud.com>
2026-08-30 02:15:50 +0000

    refactor(web): moved the authentication and sessionstorage logic out of the frontend webpage to utils auth

--- web/app/auth/login/page.tsx
+++ web/app/auth/login/page.tsx
@@ -1,41 +1,18 @@
 'use client';
 
 import useApiUrl from '@/app/hooks/useApiUrl';
-import { isTesting } from '@/app/utils/env';
-import { login as loginSdk } from '@rehua/sdk/functional/auth';
+import { login, sessionStorageAddUserInfo } from '@/app/utils/auth';
 import { useMutation } from '@tanstack/react-query';
 import Form from 'next/form';
 import type { JSX } from 'react';
-import typia, { functional } from 'typia';
-
-async function login({
-  host,
-  formData,
-}: {
-  host: string;
-  formData: FormData;
-}): Promise<loginSdk.Output> {
-  return loginSdk(
-    { host, simulate: isTesting, options: { credentials: 'include' } },
-    {
-      userName: typia.assert<string>(formData.get('userName')),
-      password: typia.assert<string>(formData.get('password')),
-      totpCode: typia.assert<string>(formData.get('totpCode')),
-    },
-  );
-}
+import { functional } from 'typia';
 
 function Home(): JSX.Element {
   const host = useApiUrl();
 
   const loginMutation = useMutation({
     mutationFn: login,
-    onSuccess: (data) => {
-      sessionStorage.setItem('firstName', JSON.stringify(data.firstName));
-      sessionStorage.setItem('lastName', JSON.stringify(data.lastName));
-      sessionStorage.setItem('userName', JSON.stringify(data.username));
-      sessionStorage.setItem('group', JSON.stringify(data.group));
-    },
+    onSuccess: sessionStorageAddUserInfo,
   });
 
   return (
