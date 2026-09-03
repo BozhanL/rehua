ecf4c0ec8d39961ce1dc5250cedcf6494d9248f5
waniven <waniven@icloud.com>
2026-09-03 12:44:40 +0000

    fix(api): added reflector to jwtguard

--- api/src/auth/jwt.guard.ts
+++ api/src/auth/jwt.guard.ts
@@ -1,6 +1,23 @@
+import { IS_PUBLIC_KEY } from './auth.controller';
 import { JWT_STRATEGY_NAME } from './jwt.strategy';
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard(JWT_STRATEGY_NAME) {}
+export class JwtAuthGuard extends AuthGuard(JWT_STRATEGY_NAME) {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  override canActivate(context: ExecutionContext): boolean {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+    return super.canActivate(context) as boolean;
+  }
+}
