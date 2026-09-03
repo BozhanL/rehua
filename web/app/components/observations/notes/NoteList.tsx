21dde7ab0e0fa830c56072bddcf9af8ddf1fbcd1
as178 <shadowangel753@gmail.com>
2026-09-03 06:27:46 +0000

    feat(web): added text for case of zero notes being present

--- web/app/components/observations/notes/NoteList.tsx
+++ web/app/components/observations/notes/NoteList.tsx
@@ -52,61 +52,71 @@ function NotesList({
   return (
     <div className="flex flex-col gap-4">
       {/* iterate through sorted notes */}
-      {sortedNotes.map((note) => (
-        <div key={note.noteId} className="flex flex-col gap-3 p-4">
-          {/* display note author and creation date */}
-          <div className="flex items-start justify-between">
-            <div className="text-xl font-bold">
-              {note.authorName}
-              <br />
-              {formatDate(note.createdAt)}
-            </div>
-          </div>
-
-          {/* note content, in formatted HTML, height depends on number of lines */}
-          <div
-            className="rounded-md border p-3 text-xl"
-            style={{
-              minHeight: 'auto',
-              boxShadow: 'inset 0 1px 3px rgb(0 0 0 / 0.3)',
-            }}
-            dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(note.html) }} // render sanitised html content to see formatting changes
-          />
-
-          {/* edit button & version history button */}
-          <div className="flex justify-start gap-3">
-            <ContentButton
-              text1="Edit Note"
-              iconProps={{ name: 'pencil' }}
-              backgroundColor="bg-rehua-orange"
-              textIconGap={0.3}
-              verticalPadding={0.2}
-              onClick={() => {
-                onEditFormatting(note);
-              }}
-            />
-            {Boolean(note.auditHistory?.length) && (
-              <ContentButton
-                text1="Previous Audits"
-                iconProps={{ name: 'version-history' }}
-                backgroundColor="bg-rehua-red"
-                textIconGap={0.3}
-                verticalPadding={0.2}
-                onClick={() => {
-                  onViewAuditHistory(note);
-                }}
-              />
-            )}
-          </div>
-
-          {/* footer of each note */}
-          <div className="border-b text-base font-semibold text-rehua-maroon">
-            {note.lastFormattedAt && note.lastFormattedBy
-              ? `Last updated by ${note.lastFormattedBy} on ${formatDate(note.lastFormattedAt)}`
-              : 'No audits have been made to this note yet.'}
-          </div>
+      {sortedNotes.length === 0 ? (
+        <div className="px-4 py-10 text-center text-xl">
+          No notes available.
         </div>
-      ))}
+      ) : (
+        <>
+          {sortedNotes.map((note) => (
+            <div key={note.noteId} className="flex flex-col gap-3 p-4">
+              {/* display note author and creation date */}
+              <div className="flex items-start justify-between">
+                <div className="text-xl font-bold">
+                  {note.authorName}
+                  <br />
+                  {formatDate(note.createdAt)}
+                </div>
+              </div>
+
+              {/* note content, in formatted HTML, height depends on number of lines */}
+              <div
+                className="rounded-md border p-3 text-xl"
+                style={{
+                  minHeight: 'auto',
+                  boxShadow: 'inset 0 1px 3px rgb(0 0 0 / 0.3)',
+                }}
+                dangerouslySetInnerHTML={{
+                  __html: DOMPurify.sanitize(note.html),
+                }} // render sanitised html content to see formatting changes
+              />
+
+              {/* edit button & version history button */}
+              <div className="flex justify-start gap-3">
+                <ContentButton
+                  text1="Edit Note"
+                  iconProps={{ name: 'pencil' }}
+                  backgroundColor="bg-rehua-orange"
+                  textIconGap={0.3}
+                  verticalPadding={0.2}
+                  onClick={() => {
+                    onEditFormatting(note);
+                  }}
+                />
+                {Boolean(note.auditHistory?.length) && (
+                  <ContentButton
+                    text1="Previous Audits"
+                    iconProps={{ name: 'version-history' }}
+                    backgroundColor="bg-rehua-red"
+                    textIconGap={0.3}
+                    verticalPadding={0.2}
+                    onClick={() => {
+                      onViewAuditHistory(note);
+                    }}
+                  />
+                )}
+              </div>
+
+              {/* footer of each note */}
+              <div className="border-b text-base font-semibold text-rehua-maroon">
+                {note.lastFormattedAt && note.lastFormattedBy
+                  ? `Last updated by ${note.lastFormattedBy} on ${formatDate(note.lastFormattedAt)}`
+                  : 'No audits have been made to this note yet.'}
+              </div>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 }
