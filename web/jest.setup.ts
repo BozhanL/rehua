80796d8090b4908f85a268e4252c89ac6bedb4a4
Bozhan Liang <lbz@rivensbane.com>
2026-09-22 03:20:38 +0000

    fix(web): fix test error

--- web/jest.setup.ts
+++ web/jest.setup.ts
@@ -2,5 +2,17 @@ import { jest } from '@jest/globals';
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/jest-globals';
 import nextRouterMock from 'next-router-mock/navigation';
+import { TextDecoder, TextEncoder } from 'node:util';
 
 jest.mock('next/navigation', () => nextRouterMock);
+
+Object.defineProperties(globalThis, {
+  TextEncoder: {
+    value: TextEncoder,
+    configurable: true,
+  },
+  TextDecoder: {
+    value: TextDecoder,
+    configurable: true,
+  },
+});
